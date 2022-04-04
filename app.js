f67285f1513e23ed20b204e7f722dff2cf9c2fe6
Rubas <ruben.lozano1195@gmail.com>
2022-04-04 10:41:42 -0500

    Primer commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const { crearArchivo } = require('./helpers/multiplicar');
+require('colors');
+const argv = require('./config/yargs');
+
+
+
+
+console.clear();
+
+
+
+crearArchivo(argv.b, argv.l, argv.h)
+    .then(nombreArchivo => console.log(nombreArchivo.rainbow, 'Creado'))
+    .catch(err => console.log(err));
